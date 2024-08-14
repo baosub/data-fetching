d48c30f9d1f980b2d5db127bf08221909d4e3a4c
Baldo <barrosoub@protonmail.com>
2024-08-14 11:59:11 -0600

    an example of data fetching was added, using SWR librery to fetch data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,37 @@
+'use client'
+
 import React from 'react'
+import useSWR from 'swr';
+
+// Función para obtener datos desde la API
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const page = () => {
+
+  // Usando SWR para obtener datos
+  const { data, error } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher);
+
+  // Imprimir en la consola del navegador
+  if (data) {
+    console.log('Datos obtenidos:', data);
+  }
+
+  // Manejo de errores
+  if (error) return <div>Error al cargar los datos</div>;
+  if (!data) return <div>Cargando...</div>;
+
+  //Renderizado de los datos obtenidos
   return (
     <div>
-      this is home
-
+      <h1>Lista de Posts</h1>
+      <ul>
+        {data.map((post: any) => (
+          <li key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
